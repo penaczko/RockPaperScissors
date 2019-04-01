14bcedc1e91d41190e754c6f431ad8bf6806a3ec
Penaczko <bartosz.pekalski@gmail.com>
2019-04-01 20:07:34 +0200

    Improve logic of game system

--- src/app/control/game.service.ts
+++ src/app/control/game.service.ts
@@ -6,73 +6,82 @@ import {ResultService} from '../model/result.service';
 })
 export class GameService {
   messageOut: string;
-  compChoice: number;
-  rockPaperScissors = [1 , 2 , 3];
+  compChoice: string;
+  userChoice: string;
+  // '1' - Paper , '2' - Rock , '3' - Scissors
+  rockPaperScissors = ['r' , 'p' , 's'];
 
   constructor(private gameResult: ResultService) { }
 
-  Play(userChoice: string) {
-    // '1' - Paper , '2' - Rock , '3' - Scissors
+  win() {
+    console.log('Win');
+    this.messageOut = 'You WON!';
+    this.gameResult.addUserPoint();
+    document.getElementById(this.userChoice).classList.add('green-glow');
+    setTimeout(() => {document.getElementById(this.userChoice).classList.remove('green-glow')}, 1000 );
+  }
+
+  lose() {
+    console.log('Lose');
+    this.messageOut = 'You LOSE!';
+    this.gameResult.addCompPoint();
+    document.getElementById(this.userChoice).classList.add('red-glow');
+    setTimeout(() => {document.getElementById(this.userChoice).classList.remove('red-glow')}, 1000 );
+  }
+
+  draw() {
+    console.log('Draw');
+    this.messageOut = 'It is DRAW!';
+    document.getElementById(this.userChoice).classList.add('gray-glow');
+    setTimeout(() => {document.getElementById(this.userChoice).classList.remove('gray-glow')}, 1000 );
+  }
+
+  Play(uChoice: string) {
+    this.userChoice = uChoice;
     this.compChoice = this.rockPaperScissors[Math.floor(Math.random() * this.rockPaperScissors.length)];
     // console.log(this.compChoice);
 
-    // For Paper:
-    if (userChoice === 'paper' && this.compChoice === 1) {
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: paper. It is draw!';
-      document.getElementById(userChoice).classList.add('gray-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('gray-glow')}, 1000 );
-    } else if (userChoice === 'paper' && this.compChoice === 2) {
-      this.gameResult.addUserPoint();
-      console.log('User won!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: rock. You won!';
-      document.getElementById(userChoice).classList.add('green-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('green-glow')}, 1000 );
-    } else if (userChoice === 'paper' && this.compChoice === 3) {
-      this.gameResult.addCompPoint();
-      console.log('Computer won!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: scissors. You lost!';
-      document.getElementById(userChoice).classList.add('red-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('red-glow')}, 1000 );
+    switch (uChoice + this.compChoice) {
+      // For Paper:
+      case 'pp': {
+        this.draw();
+        break;
+      }
+      case 'pr': {
+        this.win();
+        break;
+      }
+      case 'ps': {
+        this.lose();
+        break;
+      }
+      // For Rock:
+      case 'rr': {
+        this.draw();
+        break;
+      }
+      case 'rp': {
+        this.lose();
+        break;
+      }
+      case 'rs': {
+        this.win();
+        break;
+      }
+      // For Scissors:
+      case 'sr': {
+        this.lose();
+        break;
+      }
+      case 'sp': {
+        this.win();
+        break;
+      }
+      case 'ss': {
+        this.draw();
+        break;
+      }
     }
-    // For Rock:
-    if (userChoice === 'rock' && this.compChoice === 1) {
-      this.gameResult.addCompPoint();
-      console.log('Computer won!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: paper. You lost!';
-      document.getElementById(userChoice).classList.add('red-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('red-glow')}, 1000 );
-    } else if (userChoice === 'rock' && this.compChoice === 2) {
-      console.log('Draw!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: rock. It is draw!';
-      document.getElementById(userChoice).classList.add('gray-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('gray-glow')}, 1000 );
-    } else if (userChoice === 'rock' && this.compChoice === 3) {
-      this.gameResult.addUserPoint();
-      console.log('User won!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: scissors. You won!';
-      document.getElementById(userChoice).classList.add('green-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('green-glow')}, 1000 );
-    }
-    // For Scissors:
-    if (userChoice === 'scissors' && this.compChoice === 1) {
-      this.gameResult.addUserPoint();
-      console.log('User won!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: paper. You won!';
-      document.getElementById(userChoice).classList.add('green-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('green-glow')}, 1000 );
-    } else if (userChoice === 'scissors' && this.compChoice === 2) {
-      this.gameResult.addCompPoint();
-      console.log('Computer won!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: rock. You lost!';
-      document.getElementById(userChoice).classList.add('red-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('red-glow')}, 1000 );
-    } else if (userChoice === 'scissors' && this.compChoice === 3) {
-      console.log('Draw!');
-      this.messageOut = 'User choice: ' + userChoice + '. Comp choice: scissors. It is draw!';
-      document.getElementById(userChoice).classList.add('gray-glow');
-      setTimeout(() => {document.getElementById(userChoice).classList.remove('gray-glow')}, 1000 );
-    }
-
     console.log(this.gameResult.getResult());
   }
 }
